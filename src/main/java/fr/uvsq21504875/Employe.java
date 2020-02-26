77a691fae8d3348934264febf13496da5bae0324
Elarif Hamidou <elarif.hamidou@ens.uvsq.fr>
2020-02-26 12:23:56 +0100

    Création projet Maven + classe Employe (abstraite)

---
+++ src/main/java/fr/uvsq21504875/Employe.java
@@ -0,0 +1,16 @@
+package fr.uvsq21504875;
+
+public abstract class Employe{
+
+    protected int salaire=1500;
+    protected int anciennete;
+
+    public Employe(int anciennete){
+        this.anciennete=anciennete;
+    }
+
+    public int calculSalaire(){
+        return salaire+anciennete*20;
+    }
+
+}
